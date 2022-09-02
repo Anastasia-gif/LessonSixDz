c28003ebe98e47523eb1ee20ed80246c3914eed4
Анастасия <nastenka_slastenka2022@mail.ru>
2022-09-03 04:28:49 +0800

    add task 68 calculating the ackerman function

---
+++ Task068_calculatingTheAckermanFunction/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+m = 2, n = 3 -> A(m,n) = 9
+
+*/
+
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Идёт выполнение программы: ");
+
+int step = 0;
+int CalculatingAckermanFunction(int m, int n)
+{
+    step++;
+
+    if (m == 0) return n + 1;
+    if (n == 0) return CalculatingAckermanFunction(m - 1, 1);
+    return CalculatingAckermanFunction(m - 1, CalculatingAckermanFunction(m, n - 1));
+
+}
+
+Console.WriteLine(CalculatingAckermanFunction(m,n));
